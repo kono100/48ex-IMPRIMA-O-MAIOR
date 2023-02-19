d22c577fcda135739ad2a1833b6203f3c03b9ed8
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-02-19 04:04:51 -0300

    Add files via upload

---
+++ 48ex IMPRIMA O MAIOR.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+#48. Crie um programa que leia dois valores inteiros. Em seguida, crie uma função que receba
+#estes valores, mas imprima na tela informando qual deles é o MAIOR.
+
+
+
+num1 = int(input("Digite 1° Número: ")) 
+num2 = int(input("Digite 2° Número: ")) 
+
+if num1 > num2:
+    print(f'\nNúmero maior é {num1}\n')
+else:
+    print(f'\nNúmero maior é {num2}\n')
